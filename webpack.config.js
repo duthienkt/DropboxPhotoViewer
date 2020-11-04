3dc38c7849f9ba5ad729e61b1f2f248062585f87
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-11-04 19:14:25 +0700

    - setting

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+
+var packages = {
+    default: {
+        entry: ["./js/app/index.js"],
+        filename: "./dist/NikMi500pxApp.js"
+    }
+}
+
+const PACKAGE = 'default';
+
+
+module.exports = {
+    target: 'web',
+    mode: process.env.MODE || "development",
+    // mode: 'production',
+    entry: packages[PACKAGE].entry,
+    output: {
+        path: path.join(__dirname, "."),
+        filename: packages[PACKAGE].filename
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, './node_modules')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { presets: [['@babel/preset-env', { modules: false }]] }
+            },
+            {
+                test: /\.(tpl|txt|xml|rels)$/i,
+                use: 'raw-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    optimization: {
+        minimize: false
+    },
+    devServer: {
+        compress: true,
+        disableHostCheck: true,
+        host: '0.0.0.0'
+    },
+    performance: {
+        hints: false
+    }
+};
